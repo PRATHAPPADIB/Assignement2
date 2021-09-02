fd339c60071ba6f20f20b0c16b27047831fdfc2e
Prathap <prathapkumar942@gmail.com>
2021-09-02 14:42:12 +0530

    ASSIGNMENT 2 Phase 1

---
+++ pattern4.java
@@ -0,0 +1,30 @@
+class pattern4 {
+
+        public static void main(String[] args) {
+                byte row = 5;
+
+                for (int i = 0; i < row; i++) {
+
+                        for (int j = 0; j <= i; j++) {
+
+                                System.out.print("* ");
+
+                        }
+
+                        System.out.println();
+                }
+
+                for (int i = 0; i <= row; i++) {
+
+                        for (int j = row; j >= i; j--) {
+
+                                System.out.print("* ");
+
+                        }
+
+                        System.out.println();
+                }
+
+        }
+
+}
---
+++ pattern7.java
@@ -0,0 +1,40 @@
+class pattern7 {
+
+        public static void main(String[] args) {
+                byte row = 5;
+
+                for (int i = 1; i <= row; i++) {
+
+                        for (int j = row; j >= i; j--) {
+
+                                System.out.print("- ");
+
+                        }
+                        for (int j = row; j >= i; j--) {
+
+                                System.out.print("* ");
+
+                        }
+
+                        System.out.println();
+
+                }
+
+                for (int i = 1; i <= row; i++) {
+
+                        for (int k = 1 ; k <= i; k++) {
+
+                                System.out.print("- ");
+
+                        }
+                        for (int k = 1 ; k <= i; k++) {
+
+                                System.out.print("* ");
+
+                        }
+
+                        System.out.println();
+
+                }
+        }
+}
